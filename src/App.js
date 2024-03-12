83fd0326fa160af0cc869812e3bd3fe17925f2fc
Kausar Hina <kausar.hina@gmail.com>
2024-03-12 02:31:14 -0600

    React Week 2 Workshop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import './App.css';
 
 function App() {
     return (
@@ -13,13 +14,16 @@ function App() {
             <Header />
             <Routes>
                 <Route path='/' element={<HomePage />} />
+                <Route path='/' element={<AboutPage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
                 <Route
                     path='directory/:campsiteId'
                     element={<CampsiteDetailPage />}
                 />
+                <Route path='about' element={<AboutPage />} />
             </Routes>
+          
             <Footer />
         </div>
     );
